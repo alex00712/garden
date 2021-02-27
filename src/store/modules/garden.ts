f8fe5a7abe387b316cc3d33b9ff5e6f03ca50902
alex00712 <alexandralexandrovich007@gmail.com>
2021-02-27 11:12:19 +0300

    fix empty image

--- src/store/modules/garden.ts
+++ src/store/modules/garden.ts
@@ -141,6 +141,10 @@ export default {
             const date: Date = new Date()
             const fileName = `${product.image.fileName}_${+date}`
             
+            if(typeof product.image==="string"){
+                product.image = {}
+            }
+
             fd.append("id", product.id)
             fd.append("active", product.active.toString())
             fd.append("name", product.name)
